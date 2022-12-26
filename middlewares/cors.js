61bf8acd3272f76fb127795059cb10ca97082ad7
Sergey Penkin <p3nkinn@gmail.com>
2022-12-26 19:23:44 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    req.status(200).send();
+    // req.status(200).send();
     return res.end();
   }
   return next();
