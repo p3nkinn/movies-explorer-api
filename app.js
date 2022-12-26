50c5fe62a9e3aee9a07e3fb7b50492f93a02c632
Sergey Penkin <p3nkinn@gmail.com>
2022-12-26 18:12:27 +0300

    cors

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-const cors = require('./middlewares/cors');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/rateLimit');
 const router = require('./routes/index');
 
-const { PORT = 3010, MONGO_DB = 'mongodb://localhost:27017/moviesdb' } = process.env;
-
+const { PORT = 3010 } = process.env;
+const options = {
+  origin: [
+    'http://localhost:3010',
+    'https://p3nkinn.students.nomoredomains.sbs',
+    'https://github.com/p3nkinn/react-mesto-api-full.git',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 const app = express();
-app.use(cors);
+app.use('*', cors(options));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-mongoose.connect(MONGO_DB);
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
